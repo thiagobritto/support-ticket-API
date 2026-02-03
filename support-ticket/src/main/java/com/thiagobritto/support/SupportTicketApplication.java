8c8dad182b1626b5f00076d174302971ce4d8c7b
thiagobritto <thiagobritto.developer@gmail.com>
2026-02-03 19:46:49 -0300

    feat: initialize support ticket API project

---
+++ support-ticket/src/main/java/com/thiagobritto/support/SupportTicketApplication.java
@@ -0,0 +1,13 @@
+package com.thiagobritto.support;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SupportTicketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SupportTicketApplication.class, args);
+	}
+
+}
---
+++ support-ticket/src/test/java/com/thiagobritto/support/SupportTicketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thiagobritto.support;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SupportTicketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
